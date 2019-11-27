e6a7eb9718043fdb30c1c0ff8bb23d2c5443d608
vivianzhhh <zh1272@nyu.edu>
2019-11-27 15:51:00 -0500

    Update train.py

--- train.py
+++ train.py
@@ -46,13 +46,12 @@ num_examples = 64
 ROTATE_NUM = 4 
 
 # get discriminator obj and generator obj
-discriminator = Discriminator()
-generator = Generator()
+discriminator = Discriminator(3)
+generator = Generator(128,3,32)
 
 # optimizer
 optimizer_G = torch.optim.Adam(discriminator.parameters(), lr = 0.0002)
 optimizer_D = torch.optim.Adam(generator.parameters(), lr = 0.0002)
-
 
 # training
 for epoch in range(epochs):
@@ -61,7 +60,7 @@ for epoch in range(epochs):
 		
 		N = real_batch.size(0)
 
-        # prepare real data and fake data
+                # prepare real data and fake data
 		real_data = Variable(real_batch)
 		fake_data = generator(noise(N))
 
@@ -71,10 +70,10 @@ for epoch in range(epochs):
 		# true/false loss for G
 		G_loss = loss(discriminator(fake_data)[0], zeros_target(N))
         
-        '''
-        Compute rotation loss
-        1. generate images with rotations : 0 90 180 270
-        2. compute loss  
+               '''
+               Compute rotation loss
+               1. generate images with rotations : 0 90 180 270
+               2. compute loss  
 		'''
 		fake_data90 = torch.rot90(fake_data, 3, [2,3])
 		fake_data180 = torch.rot90(fake_data, 2, [2,3])
@@ -116,9 +115,9 @@ for epoch in range(epochs):
 		D_loss = D_loss_real + D_loss_fake
 
 		'''
-        Compute rotation loss
-        1. generate images with rotations : 0 90 180 270
-        2. compute loss  
+                Compute rotation loss
+                1. generate images with rotations : 0 90 180 270
+                2. compute loss  
 		'''
 		real_data90 = torch.rot90(real_data, 3, [2,3])
 		real_data180 = torch.rot90(real_data, 2, [2,3])
@@ -132,15 +131,14 @@ for epoch in range(epochs):
 		D_loss = beta * D_rot_loss + D_loss
 
 		D_loss.backward()
-        optimizer_D.step()
+                optimizer_D.step()
 
-        # print loss and accuracy
-        if nth_batch % 100 == 0:
-        	print('Training epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                # print loss and accuracy
+                if nth_batch % 100 == 0:
+			print('Training epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
         		epoch, nth_batch * len(real_batch), len(train_loader.dataset),
         		100. * nth_batch / len(train_loader), D_loss.item() 
-        		)
-        	)
+        		))
 
 
 
