f26e4c1648ed4bee58247d6e7704e2b4b46ea238
vivianzhhh <zh1272@nyu.edu>
2019-11-27 15:33:40 -0500

    Update Generator

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.funcitonal as F
-from resblock import ResidualBlock
+from resblock import G_ResidualBlock
 
 class Generator(nn.Module):
 
-	def __init__(self, z_size, output_channel, resnet = False, output_size = 32):
+	def __init__(self, z_size, output_channel, output_size = 32):
 
 		super(Generator,self).__init__()
 
@@ -14,27 +14,25 @@ class Generator(nn.Module):
 		self.output_size = output_size
 		self.s = s
 		self.z_size = z_size
-		self.resnet = resnet
 
 		self.res1 = G_ResidualBlock(256,256, upsample = True)
 		self.res2 = G_ResidualBlock(256,256, upsample = True)
 		self.res3 = G_ResidualBlock(256,256, upsample = True)
-
-        self.fc = nn.Linear(128,s*s*256)
+		
+                self.fc = nn.Linear(128,s*s*256)
 		self.bn = nn.BatchNorm2d(256)
 		self.relu = nn.ReLU()
 		self.tanh = nn.Tanh()
-		self.conv = nn.Conv2d(256,channel, padding = 1, kernel_size = 3, stride = 1)
+		self.conv = nn.Conv2d(256,output_channel, padding = 1, kernel_size = 3, stride = 1)
 
 	def forward(self,x):
-		input = x
 		x = self.fc(x)
-        x = x.view(-1, 256 * self.s * self.s)
+                x = x.view(-1, 256 * self.s * self.s)
 		x = self.res1(x)
 		x = self.res2(x)
 		x = self.res3(x)
 		x = self.bn(x)
-		x = F.relu(x)
+		x = self.relu(x)
 		x = self.conv(x)
 		x = self.tahn(x)
 		return x
