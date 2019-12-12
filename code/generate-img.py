ee40ce7c6848a03e6f5796a283aee39fce236d5b
vivianzhhh <zh1272@nyu.edu>
2019-12-11 19:33:09 -0500

    generate fake images

--- code/generate-img.py
+++ code/generate-img.py
@@ -1,28 +1,32 @@
-import torch
 from utils import noise
-import cv2
+import torchvision.utils
+from dataloaders import get_cifar_dataloaders
+from Model import Generator
+#import cv2
 
 # reload generator model
 def load_generator(filepath):
-    model = torch.load(filepath)
-    model.load_state_dict(checkpoint['state_dict'])
+    model = Generator(128,3)
+    if torch.cuda.is_available():
+        model = model.cuda()
+    paras = torch.load(filepath)
+    model.load_state_dict(paras)
     for para in model.parameters():
         para.requires_grad = False
-    
+
     model.eval()
     return model
 
-
-filepath = '../results1/G_cifar.pt'
+filepath = 'results1/G_cifar.pt'
 generator = load_generator(filepath)
+_, test_loader = get_cifar_dataloaders()
 num_data = len(test_loader.dataset)
 
-# generate tensor and write to img
+# store img in generated_img folder
+folder = './generated_img/'
+# generator tensor
 for i in range(num_data):
     output = generator(noise(128,1))
     print(output.size())
-    fp = '../generate-img/' + str(i+1) + 'th_img' + '.png'
-    output = output.numpy()
-    cv2.imwrite(fp, output)
-
-    
+    img_name = folder + str(i+1) + 'th_img.png'
+    #cv2.imwrite(img_name, output)
